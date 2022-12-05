a79ef1bc97c7deba4a5cba063a1f88c23376441f
Serega235645 <87755400+Serega235645@users.noreply.github.com>
2022-12-05 09:30:17 +0200

    Add files via upload

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>() { 5, -1, 3, 10, -4, 6 };
+
+            Console.WriteLine("Maximum element" + list.Max());
+            Console.WriteLine("Minimum element" + list.Min());
+        }
+    }
+}
